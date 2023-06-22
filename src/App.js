5f25694379cfaf0bbb49e8b9ecbdec923756cd18
mizanur-rahman-noakhali <126378640+mizanur-rahman-noakhali@users.noreply.github.com>
2023-06-22 22:46:58 +0600

    Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import './App.css';
 import Home from './components/Home/Home';
-
+import NoMatch from './components/NoMatch/NoMatch';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div >
-     <Home> </Home>
-  
-    </div>
+   <Router>
+    <Routes>
+    <Route  path="/home" element={<Home/>}/>
+    <Route exact path="/" element={<Home/>}/>
+    { <Route  path="*" element={<NoMatch/>}/>  }
+    </Routes>
+   </Router>
   );
 }
 
